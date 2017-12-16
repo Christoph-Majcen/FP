2a5944a45f330284af02b457b4922cc453e30638
Christoph Majcen <christoph.majcen@gmx.at>
2017-12-16 13:33:04 +0100

    Fix gradle and consolidate tests

--- src/test/java/at/majcen/functions/FunctionsTest.java
+++ src/test/java/at/majcen/functions/FunctionsTest.java
@@ -1,109 +1,67 @@
 package at.majcen.functions;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Function;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class FunctionsTest {
 
 	@Test
 	public void testMap1() {
-		List<String> stringList = new ArrayList<>(5);
-		stringList.add("Hello");
-		stringList.add("darkness");
-		stringList.add("my");
-		stringList.add("old");
-		stringList.add("friend");
+		List<String> stringList = List.of("Hello", "darkness", "my", "old", "friend");
 
 		List<Integer> result = Functions.map(stringList, String::length);
-		List<Integer> expected = new ArrayList<>(5);
-		expected.add(5);
-		expected.add(8);
-		expected.add(2);
-		expected.add(3);
-		expected.add(6);
 
-		Assert.assertEquals(expected, result);
+		assertThat(result).containsExactly(5, 8, 2, 3, 6);
 	}
 
 	@Test
 	public void testMap2() {
-		List<Integer> stringList = new ArrayList<>(5);
-		stringList.add(1);
-		stringList.add(2);
-		stringList.add(3);
-		stringList.add(4);
-		stringList.add(5);
+		List<Integer> intList = List.of(1, 2, 3, 4, 5);
 
-		List<Double> result = Functions.map(stringList, Integer::doubleValue);
-		List<Double> expected = new ArrayList<>(5);
-		expected.add(1.);
-		expected.add(2.);
-		expected.add(3.);
-		expected.add(4.);
-		expected.add(5.);
+		List<Double> result = Functions.map(intList, Integer::doubleValue);
 
-		Assert.assertEquals(expected, result);
+		assertThat(result).containsExactly(1., 2., 3., 4., 5.);
 	}
 
 	@Test
 	public void testMap3() {
-		List<String> stringList = new ArrayList<>(5);
-		stringList.add("12");
-		stringList.add("23");
-		stringList.add("42");
-		stringList.add("89");
-		stringList.add("14");
+		final List<String> stringList = List.of("12", "23", "42", "89", "14");
 
 		List<Integer> result = Functions.map(stringList, Integer::parseInt);
-		List<Integer> expected = new ArrayList<>(5);
-		expected.add(12);
-		expected.add(23);
-		expected.add(42);
-		expected.add(89);
-		expected.add(14);
 
-		Assert.assertEquals(expected, result);
+		assertThat(result).containsExactly(12, 23, 42, 89, 14);
 	}
 
 	@Test
 	public void testFold1() {
-		List<String> stringList = new ArrayList<>();
-		stringList.add("Hello ");
-		stringList.add("darkness ");
-		stringList.add("my ");
-		stringList.add("old ");
-		stringList.add("friend");
+		List<String> stringList = List.of("Hello ", "darkness ", "my ", "old ", "friend");
+
 		String result = Functions.fold(stringList, "", (string1, string2) -> string1 + string2);
-		Assert.assertEquals("Hello darkness my old friend", result);
+
+		assertThat("Hello darkness my old friend").isEqualTo(result);
 	}
 
 	@Test
 	public void testFold2() {
-		List<Integer> integerList = new ArrayList<>();
-		integerList.add(1);
-		integerList.add(2);
-		integerList.add(3);
-		integerList.add(4);
-		integerList.add(5);
-		int result = Functions.fold(integerList, 0, (int1, int2) -> int1 + int2);
-		Assert.assertEquals(15, result);
+		List<Integer> intList = List.of(1, 2, 3, 4, 5);
+
+		int result = Functions.fold(intList, 0, (int1, int2) -> int1 + int2);
+
+		assertThat(result).isEqualTo(15);
 	}
 
 	@Test
 	public void testFold3() {
-		List<Double> doubleList = new ArrayList<>();
-		doubleList.add(1.);
-		doubleList.add(2.);
-		doubleList.add(3.);
-		doubleList.add(4.);
-		doubleList.add(5.);
+		List<Double> doubleList = List.of(1., 2., 3., 4., 5.);
 
 		double result = Functions.fold(doubleList, 1., (double1, double2) -> double1 * double2);
-		Assert.assertEquals(120., result, 0.0001);
+
+		assertThat(result).isEqualTo(120.);
 	}
 
 	@Test
@@ -120,9 +78,8 @@ public class FunctionsTest {
 		final List<Integer> integers = List.of(5, 7, 3, 9, 11, 1, 8, 15, 3, 17);
 
 		List<Integer> actual = Functions.divideAndConquer(trivial, solve, divide, combine, integers);
-		final List<Integer> expected = List.of(1, 3, 3, 5, 7, 8, 9, 11, 15, 17);
 
-		Assert.assertEquals(expected, actual);
+		assertThat(actual).containsExactly(1, 3, 3, 5, 7, 8, 9, 11, 15, 17);
 	}
 
 	@Test
@@ -139,10 +96,9 @@ public class FunctionsTest {
 
 		final Functions.Pair<Integer> input = new Functions.Pair<>(3, 16);
 
-		final Integer actual = Functions.divideAndConquer(trivial,solve,divide,combine,input);
-		final Integer expected = 43046721;
+		final Integer actual = Functions.divideAndConquer(trivial, solve, divide, combine, input);
 
-		Assert.assertEquals(expected,actual);
+		assertThat(actual).isEqualTo(43046721);
 
 	}
 
