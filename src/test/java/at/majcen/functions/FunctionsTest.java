0fdf9f2fee6be48f3e6abdd90f5d8bffa0e058f2
kirchhof <kirchhof@technikum-wien.at>
2017-12-23 12:43:37 +0100

    added tests + refactoring

--- src/test/java/at/majcen/functions/FunctionsTest.java
+++ src/test/java/at/majcen/functions/FunctionsTest.java
@@ -91,42 +91,111 @@ public class FunctionsTest {
 		assertThat(result).containsExactly("Hello ", "Hello darkness ", "Hello darkness my ", "Hello darkness my old ", "Hello darkness my old friend");
 	}
 
-	@Test
-	public void testDivideAndConquer_QuickSort() {
-		final Function<List<Integer>, Boolean> trivial = integers -> integers.size() <= 1;
-		final Function<List<Integer>, List<Integer>> solve = integer -> integer;
-		final Function<List<Integer>, Functions.Pair<List<Integer>>> divide = integers -> Functions.divideByPivot((integer) -> integer < integers.get(0), integers);
-		final Function<Functions.Pair<List<Integer>>, List<Integer>> combine = listPair -> {
+	private Function<List<Integer>, Boolean> getQuickSortTrivial() {
+		return integers -> integers.size() <= 1;
+	}
+
+	private Function<List<Integer>, List<Integer>> getQuickSortSolve() {
+		return integer -> integer;
+	}
+
+	private Function<List<Integer>, Functions.Pair<List<Integer>>> getQuickSortDivide() {
+		return integers -> Functions.divideByPivot((integer) -> integer < integers.get(0), integers);
+	}
+
+	private Function<Functions.Pair<List<Integer>>, List<Integer>> getQuickSortCombine() {
+		return listPair -> {
 			List<Integer> integers = new ArrayList<>(listPair.first);
 			integers.addAll(listPair.second);
 			return integers;
 		};
+	}
 
+	private List<Integer> executeQuickSort(List<Integer> integers) {
+		final Function<List<Integer>, Boolean> trivial = getQuickSortTrivial();
+		final Function<List<Integer>, List<Integer>> solve = getQuickSortSolve();
+		final Function<List<Integer>, Functions.Pair<List<Integer>>> divide = getQuickSortDivide();
+		final Function<Functions.Pair<List<Integer>>, List<Integer>> combine = getQuickSortCombine();
+
+		return Functions.divideAndConquer(trivial, solve, divide, combine, integers);
+	}
+
+	@Test
+	public void testDivideAndConquer_QuickSort1() {
 		final List<Integer> integers = List.of(5, 7, 3, 9, 11, 1, 8, 15, 3, 17);
-
-		List<Integer> actual = Functions.divideAndConquer(trivial, solve, divide, combine, integers);
-
+		final List<Integer> actual = executeQuickSort(integers);
 		assertThat(actual).containsExactly(1, 3, 3, 5, 7, 8, 9, 11, 15, 17);
 	}
 
 	@Test
-	public void testDivideAndConquer_Power() {
-		final Function<Functions.Pair<Integer>, Boolean> trivial = integerPair -> integerPair.second == 1;
-		final Function<Functions.Pair<Integer>, Integer> solve = integerPair -> integerPair.first;
-		final Function<Functions.Pair<Integer>, Functions.Pair<Functions.Pair<Integer>>> divide = integerPair -> {
+	public void testDivideAndConquer_QuickSort2() {
+		final List<Integer> integers = List.of(6, 6, 6, 6, 6, 6, 6, 6, 6, 6);
+		final List<Integer> actual = executeQuickSort(integers);
+		assertThat(actual).containsExactly(6, 6, 6, 6, 6, 6, 6, 6, 6, 6);
+	}
+
+	@Test
+	public void testDivideAndConquer_QuickSort3() {
+		final List<Integer> integers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+		List<Integer> actual = executeQuickSort(integers);
+		assertThat(actual).containsExactly(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+	}
+
+	private Function<Functions.Pair<Integer>, Boolean> getPowerTrivial() {
+		return integerPair -> integerPair.second == 1 || integerPair.second == 0;
+	}
+
+	private Function<Functions.Pair<Integer>, Integer> getPowerSolve() {
+		return integerPair -> {
+			if(integerPair.second == 0) {
+				return 1;
+			}
+			return integerPair.first;
+		};
+	}
+
+	private Function<Functions.Pair<Integer>, Functions.Pair<Functions.Pair<Integer>>> getPowerDivide() {
+		return integerPair -> {
 			final Integer base = integerPair.first;
 			final Integer pow = integerPair.second;
 			final Integer powDiv = pow / 2;
 			return new Functions.Pair<>(new Functions.Pair<>(base, powDiv), new Functions.Pair<>(base, pow - powDiv));
 		};
-		final Function<Functions.Pair<Integer>, Integer> combine = integerPair -> integerPair.first * integerPair.second;
+	}
 
+	private Function<Functions.Pair<Integer>, Integer> getPowerCombine() {
+		return integerPair -> integerPair.first * integerPair.second;
+	}
+
+	// Note: Power only works for positive Integers
+	private Integer executePower(Functions.Pair<Integer> input) {
+		final Function<Functions.Pair<Integer>, Boolean> trivial = getPowerTrivial();
+		final Function<Functions.Pair<Integer>, Integer> solve = getPowerSolve();
+		final Function<Functions.Pair<Integer>, Functions.Pair<Functions.Pair<Integer>>> divide = getPowerDivide();
+		final Function<Functions.Pair<Integer>, Integer> combine = getPowerCombine();
+
+		return Functions.divideAndConquer(trivial, solve, divide, combine, input);
+	}
+
+	@Test
+	public void testDivideAndConquer_Power1() {
 		final Functions.Pair<Integer> input = new Functions.Pair<>(3, 16);
-
-		final Integer actual = Functions.divideAndConquer(trivial, solve, divide, combine, input);
-
+		final Integer actual = executePower(input);
 		assertThat(actual).isEqualTo(43046721);
+	}
 
+	@Test
+	public void testDivideAndConquer_Power2() {
+		final Functions.Pair<Integer> input = new Functions.Pair<>(4, 3);
+		final Integer actual = executePower(input);
+		assertThat(actual).isEqualTo(64);
+	}
+
+	@Test
+	public void testDivideAndConquer_Power3() {
+		final Functions.Pair<Integer> input = new Functions.Pair<>(3, 0);
+		final Integer actual = executePower(input);
+		assertThat(actual).isEqualTo(1);
 	}
 
 }
